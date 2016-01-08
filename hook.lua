31f88b512112bff99ddcb8b9dc5877f447d4806f
zhengji <zhengji@youmi.net>
2016-01-08 11:26:23 +0800

    支持多个servername

--- hook.lua
+++ hook.lua
@@ -2,7 +2,7 @@
 -- 
 local reqmonit = require("reqmonit")
 local request_time = ngx.now() - ngx.req.start_time()
-svrname_key = ngx.var.server_name
+svrname_key = ngx.var.host
 reqmonit.stat(ngx.shared.statics_dict, svrname_key, request_time)
 if tonumber(ngx.var.status) >= 500 then
     reqmonit.stat_5xx(ngx.shared.statics_dict, svrname_key)
